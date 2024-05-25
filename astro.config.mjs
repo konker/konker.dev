e7af4ec4ad9a9752be326b35716b2a7e1a4fa817
Konrad Markus <konker@iki.fi>
2024-05-25 03:02:50 +0300

    Add lastModified

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+import { remarkModifiedTime } from './src/lib/remark/remark-modified-time.mjs';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://konker.dev',
@@ -15,9 +17,7 @@ export default defineConfig({
     domains: [],
   },
   markdown: {
-    remarkPlugins: [
-      /*remarkSectionize*/
-    ],
+    remarkPlugins: [remarkModifiedTime],
     shikiConfig: {
       theme: 'github-dark-default',
       wrap: true,
