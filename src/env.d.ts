95c3458157910ebbb49962d776e2be2126f62bd4
Konrad Markus <konker@iki.fi>
2024-06-21 18:34:34 +0300

    Add Beam analytics

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+type ImportMetaEnv = {
+  readonly SITE_ANALYTICS_BEAM_TOKEN: string;
+};
+
+type ImportMeta = {
+  readonly env: ImportMetaEnv;
+};
