f26c74959340c3e9d239193b0addcc37858ba44b
Konrad Markus <konker@iki.fi>
2024-08-23 19:18:07 +0300

    Add in-memory and momento cache middlewares

---
+++ src/contrib/cache-momento.test.ts
@@ -0,0 +1,62 @@
+import * as P from '@konker.dev/effect-ts-prelude';
+
+import { MomentoClientDeps } from '@konker.dev/momento-cache-client-effect';
+import { MockMomentoClient, TEST_MOMENTO_AUTH_TOKEN } from '@konker.dev/momento-cache-client-effect/dist/lib/test';
+import { MomentoStringCacheJson } from '@konker.dev/tiny-cache-fp/dist/momento/MomentoStringCacheJson';
+
+import * as common from '../test/test-common';
+import { Deps } from '../test/test-common';
+import * as unit from './cache-momento';
+
+export type In = { foo: 'foo' };
+
+const TEST_IN: In = { foo: 'foo' };
+
+describe('middleware/cache-momento', () => {
+  const cache = MomentoStringCacheJson();
+  let TEST_DEPS: MomentoClientDeps;
+  let oldEnv: any;
+
+  beforeAll(() => {
+    oldEnv = process.env;
+    process.env = {
+      MOMENTO_AUTH_TOKEN: TEST_MOMENTO_AUTH_TOKEN,
+    };
+  });
+  beforeEach(() => {
+    TEST_DEPS = {
+      makeMomentoClient: () => P.Effect.succeed(MockMomentoClient()),
+    };
+  });
+  afterAll(() => {
+    process.env = oldEnv;
+  });
+
+  const cacheKeyResolver = <I>(_: I) => P.Effect.succeed('CACHE_KEY');
+  const coreSpy = jest.spyOn(common, 'echoCoreInDeps');
+  // const stack = unit.middleware(cacheKeyResolver, cache)(common.echoCoreIn);
+  const stack = P.pipe(
+    common.echoCoreInDeps(common.Deps),
+    unit.middleware<In, common.Deps, any>(cacheKeyResolver, cache)
+  );
+
+  test('it should work as expected', async () => {
+    // const result1 = await ruins.fromTaskEither(stack(TEST_IN)(deps));
+    // const result2 = await ruins.fromTaskEither(stack(TEST_IN)(deps));
+    const result1 = await P.pipe(
+      stack(TEST_IN),
+      P.Effect.provideService(Deps, {}),
+      P.Effect.provideService(MomentoClientDeps, TEST_DEPS),
+      P.Effect.runPromise
+    );
+    const result2 = await P.pipe(
+      stack(TEST_IN),
+      P.Effect.provideService(Deps, {}),
+      P.Effect.provideService(MomentoClientDeps, TEST_DEPS),
+      P.Effect.runPromise
+    );
+    expect(result1).toStrictEqual({ foo: 'foo' });
+    expect(result2).toStrictEqual(result1);
+    expect(coreSpy).toHaveBeenCalledTimes(1);
+  });
+});
