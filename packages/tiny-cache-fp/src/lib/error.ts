919f9470ee00faf457641cf82973497c00f11977
Konrad Markus <konker@iki.fi>
2024-12-04 00:30:36 +0200

    Amends after import of @konker.dev/tiny-cache-fp package

--- packages/tiny-cache-fp/src/lib/error.ts
+++ packages/tiny-cache-fp/src/lib/error.ts
@@ -1,8 +1,9 @@
-import type { TinyError } from '@konker.dev/tiny-error-fp';
-import { toTinyError } from '@konker.dev/tiny-error-fp';
+import { isTinyError, TinyError, toTinyError } from '@konker.dev/tiny-error-fp';
 
 export const ERROR_TAG = 'CacheError' as const;
 export type ERROR_TAG = typeof ERROR_TAG;
 
 export type CacheError = TinyError<ERROR_TAG>;
-export const toCacheError = toTinyError<ERROR_TAG>(ERROR_TAG);
+export const CacheError = TinyError(ERROR_TAG);
+export const toCacheError = toTinyError<ERROR_TAG>(ERROR_TAG, CacheError);
+export const isCacheError = isTinyError(ERROR_TAG);
