063290b1c04ddf74df6ca32ce0bdd200590f6a97
Konrad Markus <konker@iki.fi>
2024-12-05 01:33:58 +0200

    Amends after import of @konker.dev/momento-cache-client-effect package

--- packages/momento-cache-client-effect/src/lib/error.test.ts
+++ packages/momento-cache-client-effect/src/lib/error.test.ts
@@ -4,14 +4,32 @@ describe('error', () => {
   it('should work as expected with an Error instance input', () => {
     const error = new Error('BOOM!');
     const actual = unit.toMomentoClientError(error);
-    const expected = { message: 'BOOM!', cause: error, _tag: unit.ERROR_TAG };
-    expect(actual).toStrictEqual(expected);
+
+    expect(actual.toObject()).toStrictEqual({
+      _tag: unit.ERROR_TAG,
+      name: 'Error',
+      message: 'BOOM!',
+      statusCode: -1,
+      codeTag: 'GENERAL',
+      cause: error,
+      internal: true,
+      stack: expect.stringContaining('Error'),
+    });
   });
 
   it('should work as expected with a non-Error input', () => {
     const error = 'BOOM!';
     const actual = unit.toMomentoClientError(error);
-    const expected = { message: 'BOOM!', cause: error, _tag: unit.ERROR_TAG };
-    expect(actual).toStrictEqual(expected);
+
+    expect(actual.toObject()).toStrictEqual({
+      _tag: unit.ERROR_TAG,
+      name: 'MomentoClientError',
+      message: 'BOOM!',
+      statusCode: -1,
+      codeTag: 'GENERAL',
+      cause: error,
+      internal: true,
+      stack: expect.stringContaining('Error'),
+    });
   });
 });
--- packages/momento-cache-client-effect/src/lib/error.ts
+++ packages/momento-cache-client-effect/src/lib/error.ts
@@ -1,8 +1,9 @@
-import type { TinyError } from '@konker.dev/tiny-error-fp';
-import { toTinyError } from '@konker.dev/tiny-error-fp';
+import { isTinyError, TinyError, toTinyError } from '@konker.dev/tiny-error-fp';
 
 export const ERROR_TAG = 'MomentoClientError' as const;
 export type ERROR_TAG = typeof ERROR_TAG;
 
 export type MomentoClientError = TinyError<ERROR_TAG>;
-export const toMomentoClientError = toTinyError<ERROR_TAG>(ERROR_TAG);
+export const MomentoClientError = TinyError(ERROR_TAG);
+export const toMomentoClientError = toTinyError<ERROR_TAG>(ERROR_TAG, MomentoClientError);
+export const isMomentoClientError = isTinyError(ERROR_TAG);
