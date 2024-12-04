aa74e0b14c8f0e83b33b52b258149193b8418517
Konrad Markus <konker@iki.fi>
2024-12-05 01:11:11 +0200

    Amends after import of @konker.dev/tiny-treecrawler-fp package

--- packages/tiny-treecrawler-fp/src/lib/error.ts
+++ packages/tiny-treecrawler-fp/src/lib/error.ts
@@ -1,7 +1,9 @@
-import { toTinyError } from '@konker.dev/tiny-error-fp';
+import { isTinyError, TinyError, toTinyError } from '@konker.dev/tiny-error-fp';
 
 export const ERROR_TAG = 'TinyTreeCrawlerError';
 export type ERROR_TAG = typeof ERROR_TAG;
 
-export const toTinyTreeCrawlerError = toTinyError<ERROR_TAG>(ERROR_TAG);
-export type TinyTreeCrawlerError = ReturnType<typeof toTinyTreeCrawlerError>;
+export type TinyTreeCrawlerError = TinyError<ERROR_TAG>;
+export const TinyTreeCrawlerError = TinyError(ERROR_TAG);
+export const toTinyTreeCrawlerError = toTinyError<ERROR_TAG>(ERROR_TAG, TinyTreeCrawlerError);
+export const isTinyTreeCrawlerError = isTinyError(ERROR_TAG);
