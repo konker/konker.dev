d03295e0c1231361399e4914da3f5e68ecf997af
Konrad Markus <konker@iki.fi>
2024-12-06 23:58:44 +0200

    Conversions for base configs and vitest for packages/tiny-error-fp

--- packages/tiny-error-fp/src/lib.test.ts
+++ packages/tiny-error-fp/src/lib.test.ts
@@ -1,3 +1,5 @@
+import { describe, expect, it } from 'vitest';
+
 import { TinyError } from './index';
 import * as unit from './lib';
 
---
+++ packages/tiny-error-fp/vitest.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vitest/config';
+import baseConfig from '../common-config/vitest.config-base';
+
+export default defineConfig({
+  ...baseConfig,
+});
