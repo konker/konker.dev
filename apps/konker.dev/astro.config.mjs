e9441f2cfb91a8a5b77c5c9511a13620c0889113
Konrad Markus <mail@konker.dev>
2025-01-05 16:46:47 +0200

    Add generated sriHashes.mjs to konker.dev

--- apps/konker.dev/astro.config.mjs
+++ apps/konker.dev/astro.config.mjs
@@ -1,8 +1,13 @@
+import { resolve } from 'node:path';
+
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import shield from '@kindspells/astro-shield';
 import { defineConfig } from 'astro/config';
+
+const rootDir = new URL('.', import.meta.url).pathname;
+const modulePath = resolve(rootDir, 'src', 'generated', 'sriHashes.mjs');
 
 // FIXME: this does not work with CI, assumes git repo?
 // import { remarkModifiedTime } from './src/lib/remark/remark-modified-time.mjs';
@@ -10,7 +15,16 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://konker.dev',
-  integrations: [tailwind({}), sitemap(), mdx(), shield({})],
+  integrations: [
+    tailwind({}),
+    sitemap(),
+    mdx(),
+    shield({
+      sri: {
+        hashesModule: modulePath,
+      },
+    }),
+  ],
   outDir: 'www',
   server: {
     port: 4321,
