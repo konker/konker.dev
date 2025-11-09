8f6f8e6d75fdbda6fdcea4ffb4f376c6300d66c3
Konrad Markus <mail@konker.dev>
2025-11-09 16:38:06 +0200

    Add build:prod and build:container package scripts

--- apps/backend-boilerplate.konker.dev/eslint.config.mjs
+++ apps/backend-boilerplate.konker.dev/eslint.config.mjs
@@ -2,7 +2,7 @@ import baseConfig from '@konker.dev/common-config/configs/eslint.config-base.mjs
 
 export default [
   ...baseConfig,
-  { ignores: ['dist/', 'coverage/', 'vitest.config.ts', '.sst/', 'sst.config.ts', 'sst-env.d.ts'] },
+  { ignores: ['dist/', 'coverage/', 'vitest.config.ts', '.deploy', '.sst/', 'sst.config.ts', 'sst-env.d.ts'] },
   {
     languageOptions: {
       parserOptions: {
