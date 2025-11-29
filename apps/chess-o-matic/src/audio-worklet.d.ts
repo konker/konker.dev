d15e6e5d653e494bcb57232fe3fd612f83df93ba
Konrad Markus <mail@konker.dev>
2025-11-30 01:41:10 +0200

    Checkpoint

---
+++ apps/chess-o-matic/src/audio-worklet.d.ts
@@ -0,0 +1,22 @@
+// Type definitions for AudioWorklet
+declare class AudioWorkletProcessor {
+  readonly port: MessagePort;
+  process(
+    inputs: Float32Array[][],
+    outputs: Float32Array[][],
+    parameters: Record<string, Float32Array>
+  ): boolean;
+}
+
+declare function registerProcessor(
+  name: string,
+  processorCtor: (new (
+    options?: AudioWorkletNodeOptions
+  ) => AudioWorkletProcessor) & {
+    parameterDescriptors?: AudioParamDescriptor[];
+  }
+): void;
+
+declare const sampleRate: number;
+declare const currentFrame: number;
+declare const currentTime: number;
