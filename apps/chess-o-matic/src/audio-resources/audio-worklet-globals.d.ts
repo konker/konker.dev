60e3a89de53eddf050ad2c49baf2a5ec9633a3e1
Konrad Markus <mail@konker.dev>
2026-03-11 01:14:45 +0200

    Fix type errors

---
+++ apps/chess-o-matic/src/audio-resources/audio-worklet-globals.d.ts
@@ -0,0 +1,15 @@
+declare abstract class AudioWorkletProcessor {
+  readonly port: MessagePort;
+  readonly parameters: Record<string, Float32Array>;
+
+  abstract process(
+    inputs: Array<Array<Float32Array>>,
+    outputs: Array<Array<Float32Array>>,
+    parameters: Record<string, Float32Array>
+  ): boolean;
+}
+
+declare function registerProcessor(
+  name: string,
+  processorCtor: new () => AudioWorkletProcessor
+): void;
