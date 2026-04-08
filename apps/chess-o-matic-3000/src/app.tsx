cddde5bc19bc04c735dbfb4b2f0188acb4dcc44d
Konrad Markus <mail@konker.dev>
2026-04-09 01:51:40 +0300

    Fix path issue with favicons

--- apps/chess-o-matic-3000/src/app.tsx
+++ apps/chess-o-matic-3000/src/app.tsx
@@ -24,13 +24,14 @@ export default function App(): JSX.Element {
       <Meta charset="utf-8" />
       <Meta content="width=device-width, initial-scale=1" name="viewport" />
       <Meta content="#cf4b87" name="theme-color" />
+      <Meta content="yes" name="mobile-web-app-capable" />
       <Meta content="yes" name="apple-mobile-web-app-capable" />
       <Meta content="default" name="apple-mobile-web-app-status-bar-style" />
       <Meta content="Chess-o-matic" name="apple-mobile-web-app-title" />
-      <Link href="/favicon.ico" rel="icon" sizes="any" />
-      <Link href="/icons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
-      <Link href="/icons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
-      <Link href="/icons/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+      <Link href="/images/favicon.ico" rel="icon" sizes="any" />
+      <Link href="/images/icons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+      <Link href="/images/icons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+      <Link href="/images/icons/icon-192.png" rel="apple-touch-icon" sizes="180x180" />
       <Link href="/site.webmanifest" rel="manifest" />
       <Router root={renderRouterRoot}>
         <FileRoutes />
